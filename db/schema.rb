de81b032479e1bfe252ddf38e501b64224935a02
ayane93sese <koro.115ayapi@gmail.com>
2021-03-19 15:45:26 +0900

    マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_090028) do
+ActiveRecord::Schema.define(version: 2021_03_18_103500) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.date "date"
+    t.text "text"
+    t.integer "category_id", null: false
+    t.integer "rank_id", null: false
+    t.integer "status_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username", null: false
@@ -25,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_03_17_090028) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
